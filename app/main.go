a86ad4f850189b3fb9cbb1d11d310d39d056d224
archstrap <atishnaskar1269@gmai.com>
2026-01-02 05:18:49 +0530

    Add basic input reading from stdin

--- app/main.go
+++ app/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
-
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	commandName := strings.TrimRight(command, "\n")
+	fmt.Printf("%s: command not found\n", commandName)
+
 }
