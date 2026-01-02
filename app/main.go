a518ab71d3aed7bb1cc1caaa39a131ebe19fbc3a
archstrap <atishnaskar1269@gmai.com>
2026-01-02 05:46:48 +0530

    Add echo command and refactor existing code

--- app/main.go
+++ app/main.go
@@ -7,8 +7,11 @@ import (
 	"strings"
 )
 
-func main() {
+const (
+	SPACE string = " "
+)
 
+func main() {
 	prompt, reader := "$ ", bufio.NewReader(os.Stdin)
 	repl(prompt, reader)
 }
@@ -23,10 +26,29 @@ func repl(prompt string, reader *bufio.Reader) {
 			fmt.Println(err)
 		}
 
-		commandName := strings.TrimRight(command, "\n")
-		if "exit" == commandName {
+		commandName, args := SplitCommandDetails(command)
+
+		switch commandName {
+		case "exit":
 			return
+		case "echo":
+			processEchoCommand(args)
+		default:
+			fmt.Printf("%s: command not found\n", commandName)
 		}
-		fmt.Printf("%s: command not found\n", commandName)
+
 	}
+}
+
+func SplitCommandDetails(commandDetails string) (string, []string) {
+	parts := strings.Split(strings.TrimRight(commandDetails, "\n"), SPACE)
+	if len(parts) > 1 {
+		return parts[0], parts[1:]
+	}
+
+	return parts[0], []string{}
+}
+
+func processEchoCommand(args []string) {
+	fmt.Println(strings.Join(args, SPACE))
 }
