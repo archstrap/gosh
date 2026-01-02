5f38e198484061fe064d6119c2a4fa2c85d755ab
archstrap <atishnaskar1269@gmai.com>
2026-01-02 05:32:32 +0530

    Add builtin exit command

--- app/main.go
+++ app/main.go
@@ -10,21 +10,23 @@ import (
 func main() {
 
 	prompt, reader := "$ ", bufio.NewReader(os.Stdin)
-	for {
-		repl(prompt, reader)
-	}
+	repl(prompt, reader)
 }
 
 func repl(prompt string, reader *bufio.Reader) {
 
-	fmt.Print(prompt)
-	command, err := reader.ReadString('\n')
+	for {
+		fmt.Print(prompt)
+		command, err := reader.ReadString('\n')
 
-	if err != nil {
-		fmt.Println(err)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		commandName := strings.TrimRight(command, "\n")
+		if "exit" == commandName {
+			return
+		}
+		fmt.Printf("%s: command not found\n", commandName)
 	}
-
-	commandName := strings.TrimRight(command, "\n")
-	fmt.Printf("%s: command not found\n", commandName)
-
 }
