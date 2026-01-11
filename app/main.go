94fdd0c5910ab34c85800e144d380f49742e03d3
archstrap <atishnaskar1269@gmai.com>
2026-01-11 10:28:07 +0530

    Add a shell rc file and make it synchronize with OS environment

--- app/main.go
+++ app/main.go
@@ -24,8 +24,47 @@ var (
 )
 
 func main() {
-	prompt, reader := "$ ", bufio.NewReader(os.Stdin)
+
+	if err := loadShellRC(".shellrc"); err != nil {
+		fmt.Println("Unable to open .shellrc")
+		os.Exit(1)
+	}
+
+	prompt, reader := os.Getenv("PS"), bufio.NewReader(os.Stdin)
 	repl(prompt, reader)
+}
+
+func loadShellRC(path string) error {
+
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("Unable to open %s, please check whether %s exists or not.\n", path, path)
+		return err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+
+		line := strings.TrimSpace(scanner.Text())
+
+		if "" == line || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) == 2 {
+			key := parts[0]
+			value := strings.Trim(parts[1], "'\"")
+			os.Setenv(key, value)
+		}
+
+	}
+
+	return nil
+
 }
 
 func repl(prompt string, reader *bufio.Reader) {
